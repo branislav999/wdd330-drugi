3f5e34eacb5546ec2201c1169354bad7b1384d2b
Administrator <randymoss@iMac.local>
2024-07-01 16:37:12 -0600

    kinda done

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,7 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
@@ -14,3 +12,5 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+  const productId = getParam('product');
---
+++ src/js/productDetails.mjs
@@ -0,0 +1,17 @@
+import { setLocalStorage } from "./utils.mjs";
+
+export default function productDetails(productId) {
+
+}
+
+const productData = '';
+
+function addProductToCart(product) {
+    setLocalStorage("so-cart", product);
+  }
+
+
+function renderProductDetails(){
+
+
+}
